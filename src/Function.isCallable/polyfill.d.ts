171043a42a80b2a9f49fdfa9443959dc1dbff05b
Jacob Hummer <jcbhmr@outlook.com>
2023-10-17 14:23:26 -0500

    save

--- src/Function.isCallable/polyfill.d.ts
+++ src/Function.isCallable/polyfill.d.ts
@@ -1,3 +1,10 @@
-interface FunctionConstructor {
-  isCallable(fn: any): boolean;
+declare global {
+  interface FunctionConstructor {
+    /**
+     * @see https://github.com/caitp/TC39-Proposals/blob/trunk/tc39-reflect-isconstructor-iscallable.md#functioniscallable--argument-
+     * @category Stage 0
+     */
+    isCallable(argument: any): boolean;
+  }
 }
+export {};
--- src/Function.isCallable/shim.js
+++ src/Function.isCallable/shim.js
@@ -9,12 +9,7 @@ function isClassConstructor(value) {
     return false;
   }
 }
-/**
- * @param {any} argument
- * @returns {boolean}
- * @see https://github.com/caitp/TC39-Proposals/blob/trunk/tc39-reflect-isconstructor-iscallable.md#functioniscallable--argument-
- * @category Stage 0
- */
+/** @type {FunctionConstructor["isCallable"]} */
 module.exports = function (argument) {
   return typeof argument === "function" && !isClassConstructor(argument);
 };
